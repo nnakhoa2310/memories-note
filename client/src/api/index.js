7da5f926f492a2632b023b626280c60dffbf379a
nnakhoa2310 <nnakhoa2310@gmail.com>
2022-06-06 10:53:33 +0700

    update sites

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-
-const url = "http://localhost:5000/posts";
+const url =
+  process.env.NODE_ENV !== "production"
+    ? "https://memories-note-mern.herokuapp.com/posts"
+    : "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
